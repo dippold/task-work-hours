d04003727afead163452921302ca145076252f44
Fabio.Dippold <Fabio.Dippold@NB-PROJ-FABIO.ancorabr.local>
2020-01-15 14:32:09 -0200

    Desenvolvimento do menu principal

--- src/java/org/ftd/workforce/workhours/cmds/HomeCmd.java
+++ src/java/org/ftd/workforce/workhours/cmds/HomeCmd.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.ftd.workforce.workhours.services.SecurityManager;
+import org.builderforce.tasks.persistence.enums.RULES;
+import org.ftd.workforce.workhours.enums.APP;
 import org.ftd.workforce.workhours.services.MenuService;
+import org.softwareworkforce.web.mvc.enums.MSGS;
+import org.softwareworkforce.web.mvc.enums.MVC;
 import org.softwareworkforce.web.mvc.enums.VIEWS;
 import org.softwareworkforce.web.mvc.interfaces.ICmd;
 
@@ -20,10 +25,22 @@ public class HomeCmd implements ICmd {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-
-        MenuService.getInstance().buildMenuModel(req);        
-        String nextCmd = VIEWS.HOME.getName();
         
+        RULES[] rules = {
+            RULES.PROJECT_TEAM, 
+            RULES.PROJECT_MANAGER
+        };        
+        
+        String nextCmd;
+        boolean securityValidade = SecurityManager.getInstance().validate(req, rules);
+        
+        if (!securityValidade) {
+            req.setAttribute(MVC.MSG.getName(), MSGS.INVALID_RULE + this.getClass().getSimpleName());
+            nextCmd = APP.CMD_SECURITY_LOGOUT.getValue();            
+        } else {
+            MenuService.getInstance().buildMenuModel(req);        
+            nextCmd = VIEWS.HOME.getName();
+        }
         
         return nextCmd;
     }
